597941a364f693e5433100de7d061944444f710f
Kshitij <kshitij.rai@mindcraft.in>
2025-11-15 14:26:24 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Navbar from '/components/Navbar';
+import Navbar from './components/Navbar';
 import Home from './section/Home/Home';
 import About from './section/About/About';
 import Education from './section/Education/Education';
