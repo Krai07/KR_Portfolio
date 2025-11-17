6597343671dc430fb9217eb1b59fd5bc50812a38
Kshitij <kshitij.rai@mindcraft.in>
2025-11-17 16:19:51 +0530

    fixxxxxxx

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     <div className="App">
       {isReady ? (
         <>
-          <navbar />
+          <Navbar />
           <main>
             <Home />
             <About />
